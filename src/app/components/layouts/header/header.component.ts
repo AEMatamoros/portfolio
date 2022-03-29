40632fcf7bddc319d96795ac64a9bc95bbb500f7
Alexis Matamoros <alexismatamoros1996888@gmail.com>
2022-03-29 08:21:36 -0600

    Header styles improve

--- src/app/components/layouts/header/header.component.ts
+++ src/app/components/layouts/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +9,54 @@ import { Component, OnInit } from '@angular/core';
 
 export class HeaderComponent implements OnInit {
 
-  isNavbarCollapsed=true;
+  public isNavbarCollapsed = true;
+  public dinamicIcon = "assets/icons/json.svg";
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll() {
+    let navbar = document.querySelector('.navbar') as HTMLElement;
+    let navbarMobile = document.querySelector('.navbar-nav') as HTMLElement;
+    let navigationItemType1 = document.querySelectorAll('.nav-link');
+    let navigationItemType2 = document.querySelectorAll('.dropdown-item');
+    let navigationItemType3 = document.querySelectorAll('.nav-item');
+
+    
+    
+    //console.log(window.pageYOffset)
+    if (window.pageYOffset > navbar.clientHeight) {
+      this.dinamicIcon = "assets/icons/jsonalter.svg";
+      navbar.classList.add('navbar-inverse');
+      navbarMobile.classList.add('navbar-inverse');
+      navigationItemType1.forEach(node=>{
+        node.classList.add('text-dark')
+      })
+      navigationItemType2.forEach(node=>{
+        node.classList.add('text-dark')
+      })
+      navigationItemType3.forEach(node=>{
+        node.classList.add('text-dark')
+      })
+    } else {
+      this.dinamicIcon = "assets/icons/json.svg";
+      navbar.classList.remove('navbar-inverse');
+      navbarMobile.classList.remove('navbar-inverse');
+      navigationItemType1.forEach(node=>{
+        node.classList.remove('text-dark')
+      })
+      navigationItemType2.forEach(node=>{
+        node.classList.remove('text-dark')
+      })
+      navigationItemType3.forEach(node=>{
+        node.classList.remove('text-dark')
+      })
+    }
   }
 
 }
